ca6e0cfccaa3c0b5834518deee68f7789a30bb32
Sunil <sunil016@yahoo.com>
2020-05-05 13:51:06 +0530

    LeetCode Coding Day 5

---
+++ CodingChallenge/May/FirstUnique.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(getUnique("abdefghabcd"))
+}
+
+func getUnique(s string) int {
+	length := len(s)
+	var arr [26]int
+	for i := 0; i < length; i++ {
+		arr[s[i]-'a']++
+	}
+	for i := 0; i < length; i++ {
+		if arr[s[i]-'a'] == 1 {
+			return i
+		}
+	}
+	return -1
+}
