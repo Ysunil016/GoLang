9b65155f20c0a746cb31ac535d4da0ddf3a2e4ab
Sunil <sunil016@yahoo.com>
2020-05-20 20:58:41 +0530

    LeetCode May Coding Challenge Day 20

---
+++ CodingChallenge/May/WeekThree/KthSmallestBST/Init.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+var Count int = 0
+var KthSmallest int = -1
+
+func main() {
+	_1 := TreeNode{1, nil, nil}
+	_2 := TreeNode{2, &_1, nil}
+	_4 := TreeNode{4, nil, nil}
+	_3 := TreeNode{3, &_2, &_4}
+	_6 := TreeNode{6, nil, nil}
+	root := TreeNode{5, &_3, &_6}
+	kthSmallest(&root, 3)
+	fmt.Println("Value ", KthSmallest)
+}
+
+// TreeNode ...
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func kthSmallest(root *TreeNode, k int) int {
+	Count = 0
+	KthSmallest = -1
+	preOrder(root, k)
+	return KthSmallest
+}
+
+func preOrder(root *TreeNode, K int) {
+	if root == nil {
+		return
+	}
+	preOrder(root.Left, K)
+	Count++
+	if Count == K {
+		KthSmallest = root.Val
+	}
+	preOrder(root.Right, K)
+}
