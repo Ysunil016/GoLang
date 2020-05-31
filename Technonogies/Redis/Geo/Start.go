227c6d5906a728fe7a77f174606f10fd16605cea
Sunil <sunil016@yahoo.com>
2020-05-31 11:24:29 +0530

    Redis Tracks Static and Geo

--- Technonogies/Redis/Geo/Start.go
+++ Technonogies/Redis/Geo/Start.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/go-redis/redis"
 )
@@ -18,7 +20,8 @@ var wg sync.WaitGroup
 type RedisParams struct {
 	SourceHost    string
 	SourcePort    string
-	SourceChannel string
+	GeoChannel    string
+	StaticChannel string
 }
 
 func main() {
@@ -30,7 +33,8 @@ func main() {
 	// SubPort ... PORT for Subscription.
 	var SourcePort string = "6379"
 	// SourceChannel ... Channel for GEO.
-	var SourceChannel string = ""
+	var GeoChannel string = "GO_GEO_SOURCE"
+	var StaticChannel string = "GO_STATIC_SOURCE"
 
 	for index, V := range pArgs {
 		var StringVal string = strings.ToLower(V)
@@ -41,71 +45,108 @@ func main() {
 			} else {
 				log.Fatal("Please Provide, Source-HOSTNAME after the TAG ", pArgs[index])
 			}
-		case "-ph", "source-port":
+		case "-sp", "source-port":
 			if index+1 <= len(pArgs) {
 				SourcePort = pArgs[index+1]
 			} else {
 				log.Fatal("Please Provide, Source-PORT after the TAG ", pArgs[index])
 			}
-		case "-sc", "source-channel":
+		case "-gc", "geo-channel":
 			if index+1 <= len(pArgs) {
-				SourceChannel = pArgs[index+1]
+				GeoChannel = pArgs[index+1]
 			} else {
-				log.Fatal("Please Provide, Source-CHANNEL after the TAG ", pArgs[index])
+				log.Fatal("Please Provide, Geo-CHANNEL after the TAG ", pArgs[index])
+			}
+		case "-sc", "static-channel":
+			if index+1 <= len(pArgs) {
+				StaticChannel = pArgs[index+1]
+			} else {
+				log.Fatal("Please Provide, Static-CHANNEL after the TAG ", pArgs[index])
 			}
 		}
+
 	}
 
-	if SourceChannel == "" {
-		log.Fatal("Please Provide Source Channel")
-	}
-
-	fmt.Println("Geo Source from ", SourceHost, SourcePort, " at Channel ", SourceChannel)
+	fmt.Println("Geo Source from ", SourceHost, SourcePort, " Listening to Channel ", GeoChannel, " for GEO and ", StaticChannel, "for Its Static")
 	// Making Params
 	redisParam := RedisParams{
 		SourceHost:    SourceHost,
 		SourcePort:    SourcePort,
-		SourceChannel: SourceChannel,
+		GeoChannel:    GeoChannel,
+		StaticChannel: StaticChannel,
 	}
 
 	// Now All Parameters are Set ... Handling Sub and Pub
 	RedisHandler(redisParam)
 }
 
+const (
+	// TrackLifeTime ... Defines the Expiration of Tracks in Redis
+	TrackLifeTime time.Duration = time.Minute * 3 // 3 Mins
+)
+
 // RedisHandler ... Redis Connextion Handler
 func RedisHandler(args RedisParams) {
 	// Get Instance for 2 Clients - PUB and SUB
 	client := (RedInstance{args.SourceHost, args.SourcePort}).getRedisConnection()
-	redisMessage := make(chan *redis.GeoLocation)
 
-	wg.Add(1)
-	// Get LAT-LNG from Source
-	go handleSourceClient(client, args.SourceChannel, redisMessage)
+	wg.Add(2)
+	// Get LAT-LNG and ID from Source to RedisGeo
+	go handleGeoData(client, args.GeoChannel)       // Receives Geo Data and Saves Data to Redis in Geo
+	go handleStaticData(client, args.StaticChannel) // Receives the Data and Save in Redis Store.
 	wg.Wait()
 }
 
-func storeGeoLocation(client *redis.Client, HashKey string, ID string) {
-	_, err := client.GeoAdd(context.Background(), HashKey, &redis.GeoLocation{Name: ID, Longitude: -12.32, Latitude: 34}).Result()
-	if err != nil {
-		log.Fatal(err)
-	}
+// StaticData ....
+type StaticData struct {
+	Key   string
+	Value string
 }
 
-func handleSourceClient(client *redis.Client, channel string, redisChannel chan<- *redis.GeoLocation) {
+// MarshalBinary ....
+func (m StaticData) MarshalBinary() ([]byte, error) {
+	fmt.Println(m)
+	return json.Marshal(m)
+}
+
+func handleStaticData(client *redis.Client, channel string) {
 	defer wg.Done()
 	redisMsg := client.Subscribe(context.Background(), channel).Channel()
 	for {
 		msqRec := <-redisMsg
-		msgSlice := strings.Split(msqRec.Payload, " ") // KEY ID LAT LONG
+		msgSlice := strings.Split(msqRec.Payload, "@") // ID$<DATA>
+		ID := msgSlice[0]
+		StaticData := msgSlice[1]
+		// Saving Directly in Redis ... And Applying Time Duration to IT
+		_, err := client.Set(context.Background(), ID, StaticData, TrackLifeTime).Result()
+		if err != nil {
+			log.Println("Not Able to Save Data in Redis - Static Data for ID", msgSlice[1], err)
+		} else {
+			log.Println("Saved Data in Redis - Static Data for ID")
+		}
+	}
+}
+
+func handleGeoData(client *redis.Client, channel string) {
+	defer wg.Done()
+	redisMsg := client.Subscribe(context.Background(), channel).Channel()
+	for {
+		msqRec := <-redisMsg
+		msgSlice := strings.Split(msqRec.Payload, "@") // KEY$ID$LAT$LONG
 		Lat, _ := strconv.ParseFloat(msgSlice[2], 64)
 		Lng, _ := strconv.ParseFloat(msgSlice[3], 64)
 		Key := msgSlice[0]
 		ID := msgSlice[1]
-		client.GeoAdd(context.Background(), Key, &redis.GeoLocation{
+		_, err := client.GeoAdd(context.Background(), Key, &redis.GeoLocation{
 			Name:      ID,
 			Latitude:  Lat,
 			Longitude: Lng,
-		})
+		}).Result()
+		if err != nil {
+			log.Println("Not Able to Save Geo Data", err)
+		} else {
+			log.Println("Saved Geo Data", ID)
+		}
 	}
 }
 
