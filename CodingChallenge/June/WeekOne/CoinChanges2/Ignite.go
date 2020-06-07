dddc89f978f9aa4af7fbb1ae1b48656522233856
Sunil <sunil016@yahoo.com>
2020-06-07 16:53:46 +0530

    Leetcode Coding Challenge June - Coin Changes 2

--- CodingChallenge/June/WeekOne/CoinChanges2/Ignite.go
+++ CodingChallenge/June/WeekOne/CoinChanges2/Ignite.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{1, 2, 5}
-	fmt.Println(change(5, arr))
+	arr := []int{1}
+	fmt.Println(change(5000, arr))
 }
 
 func change(amount int, coins []int) int {
 	dynamic := make([]int, amount+1)
 	dynamic[0] = 1
 	for _, c := range coins {
-		fmt.Println(c)
 		for i := c; i <= amount; i++ {
-			fmt.Println(dynamic)
 			dynamic[i] += dynamic[i-c] // From Its Remainder
 		}
 	}
