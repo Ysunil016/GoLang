56f9b1cc7750093303465970aeb6af0560cccaec
Sunil <sunil016@yahoo.com>
2020-06-23 13:47:34 +0530

    Leetcode Day 23

--- CodingChallenge/June/WeekFour/CountNodes/Ignite.go
+++ CodingChallenge/June/WeekFour/CountNodes/Ignite.go
@@ -1,6 +1,19 @@
 package main
 
-func main() {
-	
+// TreeNode ...
+type TreeNode struct {
+	val   int
+	Left  *TreeNode
+	Right *TreeNode
 }
 
+func main() {
+
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + countNodes(root.Left) + countNodes(root.Right)
+}
